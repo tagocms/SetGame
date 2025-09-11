6e77e2d2a8d19f092cfa423c28f2b8c210ca6b0c
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2025-09-11 13:32:03 -0300

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Tiago Camargo Maciel dos Santos on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
